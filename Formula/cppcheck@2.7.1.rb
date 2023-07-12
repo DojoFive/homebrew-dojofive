7e1890749507f6a625d1fb7663e120b98abae8e0
Patrick Little <patrick.little@dojofive.com>
2023-07-12 08:13:17 -0500

    Adding cppcheck @ 2.7.1
    Source https://github.com/Homebrew/homebrew-core/blob/5f7c9207a45e763d7725c87d0c3e5476ac5e0576/Formula/cppcheck.rb

---
+++ Formula/cppcheck@2.7.1.rb
@@ -0,0 +1,78 @@
+class CppcheckAT271 < Formula
+  desc "Static analysis of C and C++ code"
+  homepage "https://sourceforge.net/projects/cppcheck/"
+  url "https://github.com/danmar/cppcheck/archive/2.7.1.tar.gz"
+  sha256 "f4d4ec42eb214b113c6a7ca7aaf6bafa92c33029560223049b46ac12f17f54b8"
+  license "GPL-3.0-or-later"
+  head "https://github.com/danmar/cppcheck.git", branch: "main"
+
+  bottle do
+    sha256 ventura: "c80b238d13fb249b9a9a5e38773218a5b16d542717a7f7e80c1268b87afd980f"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "python@3.10" => [:build, :test]
+  depends_on "pcre"
+  depends_on "tinyxml2"
+
+  uses_from_macos "libxml2"
+
+  def install
+    args = std_cmake_args + %W[
+      -DHAVE_RULES=ON
+      -DUSE_MATCHCOMPILER=ON
+      -DUSE_BUNDLED_TINYXML2=OFF
+      -DENABLE_OSS_FUZZ=OFF
+      -DPYTHON_EXECUTABLE=#{Formula["python@3.10"].opt_bin}/python3.10
+    ]
+    system "cmake", "-S", ".", "-B", "build", *args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+
+    # Move the python addons to the cppcheck pkgshare folder
+    (pkgshare/"addons").install Dir.glob("addons/*.py")
+  end
+
+  test do
+    # Execution test with an input .cpp file
+    test_cpp_file = testpath/"test.cpp"
+    test_cpp_file.write <<~EOS
+      #include <iostream>
+      using namespace std;
+
+      int main()
+      {
+        cout << "Hello World!" << endl;
+        return 0;
+      }
+
+      class Example
+      {
+        public:
+          int GetNumber() const;
+          explicit Example(int initialNumber);
+        private:
+          int number;
+      };
+
+      Example::Example(int initialNumber)
+      {
+        number = initialNumber;
+      }
+    EOS
+    system "#{bin}/cppcheck", test_cpp_file
+
+    # Test the "out of bounds" check
+    test_cpp_file_check = testpath/"testcheck.cpp"
+    test_cpp_file_check.write <<~EOS
+      int main()
+      {
+      char a[10];
+      a[10] = 0;
+      return 0;
+      }
+    EOS
+    output = shell_output("#{bin}/cppcheck #{test_cpp_file_check} 2>&1")
+    assert_match "out of bounds", output
+  end
+end
